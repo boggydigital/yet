88a2f0279725347189739004f14f5cdac6eaf5c4
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-10-15 22:22:08 -0700

    Fixing invalid playlist ids just hanging. The previous commit already fixed the hang, this change exists earlier

--- get_playlist.go
+++ get_playlist.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/boggydigital/nod"
 	"github.com/boggydigital/yt_urls"
 )
@@ -16,9 +15,12 @@ func GetPlaylistVideos(playlistId string) error {
 	}
 
 	for playlist != nil {
+		if len(playlist.Videos()) == 0 {
+			break
+		}
 		for _, videoIdTitle := range playlist.Videos() {
 			if err := GetVideos(videoIdTitle.VideoId); err != nil {
-				fmt.Println(err)
+				nod.Error(err)
 			}
 		}
 		if playlist.HasContinuation() {
