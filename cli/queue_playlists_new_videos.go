f4e6357433725fd7d074aac390418e83d34e0e86
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-12-21 13:27:39 -0800

    Adding sync cmd to process new playlists videos

---
+++ cli/queue_playlists_new_videos.go
@@ -0,0 +1,63 @@
+package cli
+
+import (
+	"github.com/boggydigital/kvas"
+	"github.com/boggydigital/nod"
+	"github.com/boggydigital/yet/data"
+	"github.com/boggydigital/yet/paths"
+	"net/url"
+)
+
+func QueuePlaylistsNewVideosHandler(u *url.URL) error {
+	return QueuePlaylistsNewVideos()
+}
+
+func QueuePlaylistsNewVideos() error {
+
+	qpnva := nod.NewProgress("queueing playlists new videos...")
+	defer qpnva.End()
+
+	metadataDir, err := paths.GetAbsDir(paths.Metadata)
+	if err != nil {
+		return qpnva.EndWithError(err)
+	}
+
+	rdx, err := kvas.ReduxWriter(metadataDir, data.AllProperties()...)
+	if err != nil {
+		return qpnva.EndWithError(err)
+	}
+
+	notNewIndicatorProperties := []string{
+		data.VideosDownloadQueueProperty,
+		data.VideosWatchlistProperty,
+		data.VideoEndedProperty,
+		data.VideoProgressProperty}
+
+	for _, pdq := range rdx.Keys(data.PlaylistDownloadQueueProperty) {
+		if newVideos, ok := rdx.GetAllValues(data.PlaylistNewVideosProperty, pdq); ok && len(newVideos) > 0 {
+			for _, videoId := range newVideos {
+
+				skipVideo := false
+				// don't add videos already in download queue, watch, ended, in progress
+				for _, nnip := range notNewIndicatorProperties {
+					if rdx.HasKey(nnip, videoId) {
+						skipVideo = true
+						break
+					}
+				}
+
+				if skipVideo {
+					break
+				}
+
+				if err := rdx.AddValues(data.VideosDownloadQueueProperty, videoId, data.TrueValue); err != nil {
+					return qpnva.EndWithError(err)
+				}
+			}
+		}
+	}
+
+	qpnva.EndWithResult("done")
+
+	return nil
+}
---
+++ cli/sync.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"github.com/boggydigital/nod"
+	"net/url"
+)
+
+func SyncHandler(u *url.URL) error {
+	force := u.Query().Has("force")
+	return Sync(force)
+}
+
+func Sync(force bool) error {
+
+	sa := nod.Begin("syncing playlists subscriptions...")
+	defer sa.End()
+
+	if err := UpdatePlaylistsMetadata(); err != nil {
+		return sa.EndWithError(err)
+	}
+
+	if err := UpdatePlaylistsNewVideos(); err != nil {
+		return sa.EndWithError(err)
+	}
+
+	if err := QueuePlaylistsNewVideos(); err != nil {
+		return sa.EndWithError(err)
+	}
+
+	if err := Download(nil, true, force); err != nil {
+		return sa.EndWithError(err)
+	}
+
+	return nil
+}
