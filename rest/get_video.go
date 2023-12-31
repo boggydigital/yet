746555e81bc0150ab41bc714a734d9a6153c7d10
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-12-31 10:18:48 -0800

    Setting video files MIME type

--- rest/get_video.go
+++ rest/get_video.go
@@ -2,9 +2,11 @@ package rest
 
 import (
 	"github.com/boggydigital/yet/paths"
+	"github.com/boggydigital/yt_urls"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func GetVideo(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +26,9 @@ func GetVideo(w http.ResponseWriter, r *http.Request) {
 		absFilepath := filepath.Join(absVideosDir, file)
 
 		if _, err := os.Stat(absFilepath); err == nil {
+			if strings.HasSuffix(file, yt_urls.DefaultVideoExt) {
+				w.Header().Set("Content-Type", "video/mp4")
+			}
 			http.ServeFile(w, r, absFilepath)
 		} else {
 			http.NotFound(w, r)
