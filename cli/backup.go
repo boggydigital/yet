abce286b7d91f1778fef0137c8d2e08e99381826
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-02-01 23:37:15 -0800

    Transitioning to hogo

--- cli/backup.go
+++ cli/backup.go
@@ -1,15 +1,11 @@
 package cli
 
 import (
+	"github.com/boggydigital/hogo"
 	"github.com/boggydigital/nod"
-	"github.com/boggydigital/packer"
 	"github.com/boggydigital/pasu"
 	"github.com/boggydigital/yet/paths"
 	"net/url"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
 )
 
 const daysToPreserveFiles = 30
@@ -32,73 +28,20 @@ func Backup() error {
 		return ea.EndWithError(err)
 	}
 
-	if err := packer.Pack(amp, abp, ea); err != nil {
+	if err := hogo.Compress(amp, abp, ea); err != nil {
 		return ea.EndWithError(err)
 	}
 
 	ea.EndWithResult("done")
 
-	if err := deleteOldBackups(abp); err != nil {
-		return ea.EndWithError(err)
+	cba := nod.NewProgress("cleaning up old backups...")
+	defer cba.End()
+
+	if err := hogo.Cleanup(abp, true, cba); err != nil {
+		return cba.EndWithError(err)
 	}
 
-	return nil
-}
-
-func deleteOldBackups(dir string) error {
-
-	ofa := nod.Begin("deleting old backups...")
-	defer ofa.End()
-
-	d, err := os.Open(dir)
-	if err != nil {
-		return ofa.EndWithError(err)
-	}
-	defer d.Close()
-
-	filenames, err := d.Readdirnames(-1)
-	if err != nil {
-		return ofa.EndWithError(err)
-	}
-
-	earliest := time.Now().Add(-daysToPreserveFiles * 24 * time.Hour)
-	oldFiles := make([]string, 0)
-
-	for _, fn := range filenames {
-
-		fnse := fn
-		for filepath.Ext(fnse) != "" {
-			fnse = strings.TrimSuffix(fnse, filepath.Ext(fnse))
-		}
-		ft, err := time.Parse(nod.TimeFormat, fnse)
-		if err != nil {
-			nod.Log(err.Error())
-			continue
-		}
-
-		if ft.After(earliest) {
-			continue
-		}
-
-		oldFiles = append(oldFiles, fn)
-	}
-
-	if len(oldFiles) == 0 {
-		ofa.EndWithResult("none found")
-	} else {
-		ofa.EndWithResult("found %d old backups", len(oldFiles))
-
-		rofa := nod.NewProgress("removing old backups...")
-		rofa.TotalInt(len(oldFiles))
-		for _, fn := range oldFiles {
-			filename := filepath.Join(dir, fn)
-			if err := os.Remove(filename); err != nil {
-				return rofa.EndWithError(err)
-			}
-			rofa.Increment()
-		}
-		rofa.EndWithResult("done")
-	}
+	cba.EndWithResult("done")
 
 	return nil
 }
