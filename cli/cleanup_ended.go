c5a661f38bc8f0776587ee51a8d9a8bc274ac0a9
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-08-17 12:26:32 -0700

    Cleaning up ended video properties (except for required)
    Bumping go

--- cli/cleanup_ended.go
+++ cli/cleanup_ended.go
@@ -13,6 +13,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 )
@@ -25,6 +26,7 @@ func CleanupEndedHandler(u *url.URL) error {
 // CleanupEnded removes downloads for Ended videos that match the following conditions:
 // - video download has not been downloaded earlier
 // - at least 48 hours have passed since the ended date (unless no-delay was set)
+// Additionally CleanupEnded will remove video properties (except for title) for ended videos
 func CleanupEnded(now bool, rdx kevlar.WriteableRedux) error {
 
 	cea := nod.NewProgress("cleaning up ended media...")
@@ -79,6 +81,12 @@ func CleanupEnded(now bool, rdx kevlar.WriteableRedux) error {
 	cea.TotalInt(len(cleanupVideoIds))
 
 	for _, videoId := range cleanupVideoIds {
+
+		// cleanup properties
+		if err := cleanupProperties(videoId, rdx); err != nil {
+			return cea.EndWithError(err)
+		}
+
 		if err := removeVideoFile(videoId, absVideosDir, rdx); err != nil {
 			return cea.EndWithError(err)
 		}
@@ -167,6 +175,29 @@ func removePosters(videoId string) error {
 		} else {
 			return err
 
+		}
+	}
+	return nil
+}
+
+var preserveVideoPropertiesEnded = []string{
+	data.VideoTitleProperty,             // required for history
+	data.VideoOwnerChannelNameProperty,  // required for checking empty videos directories
+	data.VideoEndedDateProperty,         // required for cleanup
+	data.VideoEndedReasonProperty,       // required for history
+	data.VideoFavoriteProperty,          // required for cleanup
+	data.VideoDownloadCompletedProperty, // required for cleanup
+	data.VideoDownloadCleanedUpProperty, // required for cleanup
+}
+
+func cleanupProperties(videoId string, rdx kevlar.WriteableRedux) error {
+	for _, vp := range data.VideoProperties() {
+		if slices.Contains(preserveVideoPropertiesEnded, vp) {
+			continue
+		}
+
+		if err := rdx.CutKeys(vp, videoId); err != nil {
+			return err
 		}
 	}
 	return nil
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/boggydigital/yet
 
-go 1.22.5
+go 1.23.0
 
 require (
 	github.com/boggydigital/backups v0.1.3
@@ -12,11 +12,11 @@ require (
 	github.com/boggydigital/nod v0.1.21
 	github.com/boggydigital/pathways v0.1.14
 	github.com/boggydigital/yet_urls v0.1.54
-	golang.org/x/exp v0.0.0-20240707233637-46b078467d37
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
 )
 
 require (
 	github.com/boggydigital/match_node v0.1.16 // indirect
 	github.com/boggydigital/wits v0.2.3 // indirect
-	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
 )
