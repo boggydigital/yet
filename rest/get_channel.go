92b72287ce95a1fe308a2a7660bf53d254ba590a
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-05-08 13:04:53 -0700

    Disabling automatic channel refresh

--- rest/get_channel.go
+++ rest/get_channel.go
@@ -1,9 +1,9 @@
 package rest
 
 import (
-	"github.com/boggydigital/yet/data"
-	"github.com/boggydigital/yet/rest/view_models"
 	"net/http"
+
+	"github.com/boggydigital/yet/rest/view_models"
 )
 
 func GetChannel(w http.ResponseWriter, r *http.Request) {
@@ -25,11 +25,11 @@ func GetChannel(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check if the channel has no videos and refresh automatically
-	if videos, ok := rdx.GetAllValues(data.ChannelVideosProperty, channelId); !ok || len(videos) == 0 {
-		url := "/refresh_channel_videos?id=" + channelId
-		http.Redirect(w, r, url, http.StatusPermanentRedirect)
-		return
-	}
+	//if videos, ok := rdx.GetAllValues(data.ChannelVideosProperty, channelId); !ok || len(videos) == 0 {
+	//	url := "/refresh_channel_videos?id=" + channelId
+	//	http.Redirect(w, r, url, http.StatusPermanentRedirect)
+	//	return
+	//}
 
 	w.Header().Set("Content-Type", "text/html")
 
