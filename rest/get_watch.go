861719ce65eed41671f2886383a83aab73444866
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-07-14 23:07:19 -0700

    Compacting Seen enough as well

--- rest/get_watch.go
+++ rest/get_watch.go
@@ -157,7 +157,7 @@ func GetWatch(w http.ResponseWriter, r *http.Request) {
 
 	videoNavButtonsRow.Append(
 		navButton("Manage", "/manage_video?v="+videoId),
-		actionButton("Seen enough", "/end/"+videoId+"/seen-enough"),
+		actionButton("Enough", "/end/"+videoId+"/seen-enough"),
 		actionButton("Skip", "/end/"+videoId+"/skipped"),
 	)
 
