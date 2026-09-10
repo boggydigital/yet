31b05565819d811fb16f0e87ec1adf210a623a1b
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-09-10 06:51:18 -0700

    Mixing button colors at 10%

--- rest/stromponents.go
+++ rest/stromponents.go
@@ -319,7 +319,7 @@ func buttonStyles(cls ...colors.Color) []string {
 	if len(cls) > 0 {
 		for _, c := range cls {
 			bs = append(bs, "color:"+c)
-			bs = append(bs, "background-color:color-mix(in display-p3,"+c+" 15%,"+colors.Highlight+")")
+			bs = append(bs, "background-color:color-mix(in display-p3,"+c+" 10%,"+colors.Highlight+")")
 		}
 	} else {
 		bs = append(bs, "color:"+colors.Foreground, "background-color:"+colors.Highlight)
